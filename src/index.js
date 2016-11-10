7116a6f51bd31f2dda04568449ac99858320a0e0
cFUD <oleg.makedonsky@inbox.com>
2016-11-10 08:00:00 +0300

    v.0.0.2
    Added form file upload plugin

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 var self = require("sdk/self");
+const CryptoJS = require('lib/aes').CryptoJS;
+let { Cc, Ci ,Cu} = require('chrome');
 
-// a dummy function, to show how tests work.
-// to see how to test this function, look at test/test-index.js
+var buttons1 = require('sdk/ui/button/action');
+var buttons2 = require('sdk/ui/button/action');
+var tabs = require("sdk/tabs");
+var ww = require("sdk/window/utils");
+
 function dummy(text, callback) {
   callback(text);
 }
 
 exports.dummy = dummy;
-
-var buttons1 = require('sdk/ui/button/action');
-var buttons2 = require('sdk/ui/button/action');
-var tabs = require("sdk/tabs");
 
 var button1 = buttons1.ActionButton({
   id: "cfud-file-exchange-link",
@@ -20,6 +21,8 @@ var button1 = buttons1.ActionButton({
     "32": "./1.png",
     "64": "./1.png"
   },
+  badge: 0,
+  badgeColor: "#00AAAA",
   onClick: handleClick1
 });
 
@@ -35,7 +38,49 @@ var button2 = buttons2.ActionButton({
 });
 
 function handleClick1(state) {
-  tabs.open("https://cfud.biz/cryptosharing/");
+	var prompts = Cc["@mozilla.org/embedcomp/prompt-service;1"].getService(Ci.nsIPromptService);
+	
+	if (state.badge>0){
+		return -1;
+	}
+	
+	var password = Math.random().toString(36).slice(-8)+Math.random().toString(36).slice(-8)+Math.random().toString(36).slice(-8)+Math.random().toString(36).slice(-8);
+	
+	const nsIFilePicker = Ci.nsIFilePicker;
+	var fp = Cc["@mozilla.org/filepicker;1"].createInstance(nsIFilePicker);
+	
+	fp.appendFilters(nsIFilePicker.filterAll | nsIFilePicker.filterText);
+
+	var rv = fp.show();
+	if (rv == nsIFilePicker.returnOK || rv == nsIFilePicker.returnReplace) {
+		button1.badge = state.badge + 1;
+		button1.badgeColor = "#AA00AA";
+
+		var path = fp.file.path;
+		var fname = fp.file.path.replace(/^.*[\\\/]/, '');
+		var file = require("sdk/io/file");
+		var {Request} = require("sdk/request");
+
+		var params = {};
+		params.data = file.read(fp.file.path, "b");
+		Cu.import("resource://gre/modules/devtools/Console.jsm");
+		Cu.importGlobalProperties(["atob", "btoa"]);
+		params.data = btoa(params.data);
+		params.data = CryptoJS.AES.encrypt(params.data,password).toString();
+		params.name = fname;
+
+		Request({
+		  url: "https://cfud.biz/cryptosharing/uploadFile/",
+		  content: params,
+		  onComplete: function(response)
+		  {
+			var clipboard = require("sdk/clipboard");
+			clipboard.set("Key : "+password + "\r\n" + "URL : https://cfud.biz/en/cryptosharing/download/" + response.text + "\r\n");
+			button1.badge = 0;	
+			button1.badgeColor = "#00AAAA";			
+		  }
+		}).post();
+	}
 }
 
 function handleClick2(state) {
